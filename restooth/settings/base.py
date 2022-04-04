fe272098a92a86c79936c911142ad41db4e28f8d
Jonathan-geo <jonathan.geotecs@gmail.com>
2022-04-04 19:15:55 -0300

    correcao static files

--- restooth/settings/base.py
+++ restooth/settings/base.py
@@ -110,12 +110,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
