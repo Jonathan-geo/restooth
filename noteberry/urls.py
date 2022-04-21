a3e73cc819be700d9b8d1a025d2f796cd7c942e5
Jonathan-geo <jonathan.geotecs@gmail.com>
2022-04-21 16:01:18 -0300

    endpoints list compras e vendas

--- noteberry/urls.py
+++ noteberry/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from noteberry.views import MovimentacoesApiView, AlteracaoMovimentacoesApiView
+from noteberry.views import MovimentacoesApiView, AlteracaoMovimentacoesApiView, ListVendasApiView, ListComprasApiView
 urlpatterns = [
     path('movimentacoes', MovimentacoesApiView.as_view(), name='cadastro_movimentacoes'),
+    path('movimentacoes/vendas', ListVendasApiView.as_view(), name='movimentacoes_vendas'),
+    path('movimentacoes/compras', ListComprasApiView.as_view(), name='movimentacoes_compras'),
     path('movimentacoes/<str:id_cliente>', AlteracaoMovimentacoesApiView.as_view(), name='alteracao_movimentacoes'),
 ]
--- noteberry/views.py
+++ noteberry/views.py
@@ -1,7 +1,7 @@
 from rest_framework import filters
 from noteberry.models import Movimentacoes
 from noteberry.serializers import MovimentacoesSerializer
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -52,3 +52,95 @@ class AlteracaoMovimentacoesApiView(RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         return Movimentacoes.objects.filter(owner=self.request.user)
 
+
+########################################
+#---->MOVIMENTACOES.VENDAS
+########################################
+class ListVendasApiView(ListAPIView):
+    serializer_class = MovimentacoesSerializer
+    permission_classes = (IsAuthenticated,)
+    filter_backends=[DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    filterset_fields=[
+            'id_cliente',
+            'nome',
+            'data',
+            'tipoMorango',
+            'qtdMorango',
+            'precoCaixaMorango',
+            'vendaMorango',
+            'observacoes',
+            'ativo',
+        ]
+    search_fields=[
+            'id_cliente',
+            'nome',
+            'data',
+            'tipoMorango',
+            'qtdMorango',
+            'precoCaixaMorango',
+            'vendaMorango',
+            'observacoes',
+            'ativo',
+        ]
+    ordering_fields=['nome']
+
+    def get_queryset(self):
+        return Movimentacoes.objects.filter(owner=self.request.user, vendaMorango=1)
+
+
+########################################
+#---->MOVIMENTACOES.COMPRAS
+########################################
+class ListComprasApiView(ListAPIView):
+    serializer_class = MovimentacoesSerializer
+    permission_classes = (IsAuthenticated,)
+    filter_backends=[DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    filterset_fields=[
+            'id_cliente',
+            'nome',
+            'data',
+            'tipoMorango',
+            'qtdMorango',
+            'precoCaixaMorango',
+            'vendaMorango',
+            'observacoes',
+            'ativo',
+        ]
+    search_fields=[
+            'id_cliente',
+            'nome',
+            'data',
+            'tipoMorango',
+            'qtdMorango',
+            'precoCaixaMorango',
+            'vendaMorango',
+            'observacoes',
+            'ativo',
+        ]
+    ordering_fields=['nome']
+
+    def get_queryset(self):
+        return Movimentacoes.objects.filter(owner=self.request.user, vendaMorango=0)
+
+
+
+
+
+
+# class CreateAtividadeAPIView(CreateAPIView):
+#     serializer_class = AtividadeSerializer
+#     permission_classes = (IsAuthenticated,)
+
+#     def perform_create(self, serializer):
+#         return serializer.save(usuario=self.request.user)
+
+
+
+# class ListAtividadesAPIView(ListAPIView):
+#     serializer_class = AtividadeSerializer
+#     permission_classes = (IsAuthenticated,)
+
+#     def get_queryset(self):
+#         return Atividade.objects.filter(usuario=self.request.user)
